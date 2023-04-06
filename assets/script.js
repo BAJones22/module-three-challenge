92bd93c2c439accae6d0467c33f5eea7ddd23c76
Brooke Adlesperger-Jones <badles@live.com>
2023-04-06 17:39:26 -0500

    updated javascript file

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,89 @@
 //Criteria Arrays
-
-var lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numericArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialArr = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '<', '>', '/', ',', '.', '?'];
+var lowerArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numericArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArr = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "=",
+  "{",
+  "}",
+  "[",
+  "]",
+  "<",
+  ">",
+  "/",
+  ",",
+  ".",
+  "?",
+];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,37 +94,75 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+function generatePassword() {
+  let numChars = askUser();
+  var possibleChars = [];
+
+  function askUser() {
+    let numChars = prompt(
+      "Please enter your desired password length with a value between 8-128"
+    );
+  
+  if (numChars > 7 && numChars < 128) {
+    alert("Success!");
+  
+  } else 
+  if (numChars === null) {
+    alert(
+      "Input must be a number with a value between 8-128. Please try again"
+    )
+    numChars = askUser();
+  } else {
+    numChars = askUser();
+  }
+    return numChars;
+  }
+
+  let lower = confirm(
+    "Do you want your password to contain lower case letters?"
+  );
+  let upper = confirm(
+    "Do you want your password to contain upper case letters?"
+  );
+  let numeric = confirm("Do you want your password to contain numbers?");
+  let special = confirm(
+    "Do you want your password to contain special characters?"
+  );
+
+  if (lower) {
+    possibleChars=possibleChars.concat(lowerArr);
+  }
+  if (upper) {
+    possibleChars=possibleChars.concat(upperArr);
+  }
+  if (numeric) {
+    possibleChars=possibleChars.concat(numericArr);
+  }
+  if (special) {
+    possibleChars=possibleChars.concat(specialArr);
+  }
+
+  if (possibleChars.length === 0) {
+    alert("No password criteria chosen. Please try again");
+  }
+
+var finalPassword = [];
+var charsLength = possibleChars.length
+
+for (var i = 0; i < numChars; i++) {
+  var randomIndex = Math.floor(Math.random() * charsLength);
+  var randomChar = possibleChars[randomIndex];
+  finalPassword.push(randomChar);
+
+}
+
+  return finalPassword.join("");
+ 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Prompts for User Password Criteria
-askUser ();
 
-
-function askUser(){
-  let answer = prompt("Please enter your desired password length with a value between 8-128");
-  if( answer>7 && answer<128 )
-    alert("Success!")
-  else if( answer === null )
-    alert("Input must be a number with a value between 8-128. Please try again")
-  else  
-    askUserAgain();
-}
-
-function askUserAgain() {
-  let answer = prompt("Input must be a number with a value between 8-128. Please try again");
-  if( answer>7 && answer<128 ) 
-    alert("Success!")
-  else if( answer === null)
-    alert("Please try again")
-  else
-    askUserAgain();
-}
-
-let lower = confirm("Do you want your password to contain lower case letters?")
-let upper = confirm("Do you want your password to contain upper case letters?")
-let numeric = confirm("Do you want your password to contain numbers?")
-let special = confirm("Do you want your password to contain special characters?")
